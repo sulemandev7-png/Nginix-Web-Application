39bc5ad6d4d9ac906b0048b2e9ddd1a28908de30
sulemandev7-png <suleman.dev7@gmail.com>
2026-06-22 15:21:52 +0500

    changed vite file

--- Docker-Project/frontend/vite.config.js
+++ Docker-Project/frontend/vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ['devpro.biz', 'www.devpro.biz'],
-    strictPort: true,
-    watch: {
-      usePolling: true,
-      interval: 1000
-    }
-  }
 })
